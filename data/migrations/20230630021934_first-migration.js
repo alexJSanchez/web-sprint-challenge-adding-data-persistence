55aceb3f48b60ef78cfd074c54bd0eb655280350
alexjsanchez1020 <alexjsanchez1020@gmail.com>
2023-06-30 21:50:27 -0400

    stable schema fixed errors

--- data/migrations/20230630021934_first-migration.js
+++ data/migrations/20230630021934_first-migration.js
@@ -4,22 +4,22 @@
  */
 exports.up = async function (knex) {
   await knex.schema.createTable("project", (table) => {
-    table.increments("project_id").notNullable;
-    table.string("project_name", 100).notNullable;
+    table.increments("project_id").notNullable();
+    table.string("project_name", 100).notNullable();
     table.string("project_description");
     table.boolean("project_completed");
   });
   await knex.schema.createTable("resources", (table) => {
-    table.increments("resource_id").notNullable;
+    table.increments("resource_id").notNullable();
     table.string("resource_name").notNullable().unique();
     table.string("resource_description");
   });
   await knex.schema.createTable("tasks", (table) => {
-    table.increments("task_id").notNullable;
-    table.string("task_description").notNullable;
+    table.increments("task_id").notNullable();
+    table.string("task_description").notNullable();
     table.string("task_notes");
     table.string("task_completed");
-    table.string("project_id").notNullable;
+    table.string("project_id").notNullable();
   });
 };
 
@@ -29,4 +29,6 @@ exports.up = async function (knex) {
  */
 exports.down = async function (knex) {
   await knex.schema.dropTableIfExists("project");
+  await knex.schema.dropTableIfExists("resources");
+  await knex.schema.dropTableIfExists("tasks");
 };
