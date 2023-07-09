31c7bedc6e8c9dd4fbdf35f389c0dc843bce3054
alex sanchez <alexjsanchez1020@gmail.com>
2023-07-09 13:33:56 -0400

    remove seed

--- data/seeds/seed_name.js
+++ data/seeds/seed_name.js
@@ -1,23 +1,23 @@
-/**
- * @param { import("knex").Knex } knex
- * @returns { Promise<void> }
- */
+// /**
+//  * @param { import("knex").Knex } knex
+//  * @returns { Promise<void> }
+//  */
 
-exports.seed = async function (knex) {
-  // Deletes ALL existing entries
-  await knex("projects").del();
-  await knex("projects").insert([
-    {
-      project_name: "sprint1",
-      project_description: "first project",
-      project_completed: false,
-    },
-  ]);
-  await knex("resources").del();
-  await knex("resources").insert([
-    {
-      resource_name: "first resource",
-      resource_description: "descrption",
-    },
-  ]);
-};
+// exports.seed = async function (knex) {
+// 	// Deletes ALL existing entries
+// 	await knex("projects").del();
+// 	await knex("projects").insert([
+// 		{
+// 			project_name: "sprint1",
+// 			project_description: "first project",
+// 			project_completed: false,
+// 		},
+// 	]);
+// 	await knex("resources").del();
+// 	await knex("resources").insert([
+// 		{
+// 			resource_name: "first resource",
+// 			resource_description: "descrption",
+// 		},
+// 	]);
+// };
