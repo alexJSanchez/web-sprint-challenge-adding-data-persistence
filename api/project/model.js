823bd1e0d9336587dad3dc213173d69c45fc8099
alex sanchez <alexjsanchez1020@gmail.com>
2023-07-10 17:29:29 -0400

    stable after adding true or false

--- api/project/model.js
+++ api/project/model.js
@@ -4,10 +4,34 @@ const db = require("../../data/dbConfig");
 
 const getAll = () => {
 	// DO YOUR MAGIC
-	return db("projects");
+	return db("projects").then((projects) =>
+		projects.map((proj) => ({
+			...proj,
+			project_completed: proj.project_completed ? true : false,
+		}))
+	);
 };
 const createProject = async (project) => {
-	const createProject = await db("projects").insert(project).returning("*");
+	const createProject = await db("projects")
+		.insert(project)
+		.returning("*")
+		.then((projects) =>
+			projects.map((proj) => ({
+				...proj,
+				project_completed: proj.project_completed ? true : false,
+			}))
+		);
+
 	return createProject;
 };
 module.exports = { getAll, createProject };
+
+// const createProject = (project) => {
+// 	return db("projects")
+// 		.insert(project, "project_id")
+// 		.then(([project_id]) => db("projects").where({ project_id }))
+// 		.then(([proj]) => ({
+// 			...proj,
+// 			project_completed: proj.project_completed ? true : false,
+// 		}));
+// };
